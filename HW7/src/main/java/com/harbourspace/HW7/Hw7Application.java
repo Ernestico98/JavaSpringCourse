d38f5fe049ce3105d0659c64e2c610da600e1308
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-02-11 18:44:04 +0100

    Implement Interface for DB and other methods

--- HW7/src/main/java/com/harbourspace/HW7/Hw7Application.java
+++ HW7/src/main/java/com/harbourspace/HW7/Hw7Application.java
@@ -2,8 +2,10 @@ package com.harbourspace.HW7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class Hw7Application {
 
 	public static void main(String[] args) {
--- HW7/src/main/java/com/harbourspace/HW7/TodoController.java
+++ HW7/src/main/java/com/harbourspace/HW7/TodoController.java
@@ -1,5 +1,7 @@
 package com.harbourspace.HW7;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,28 +9,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/todo")
 public class TodoController {
+    @Autowired
+    private IDatabase DB;
 
-    @GetMapping("")
+    @GetMapping("/todo")
     public ResponseEntity<ArrayList<ListItem>> index() {
-        var response = new ArrayList<ListItem>();
-        response.add(new ListItem(1, 1, 4, "item1", "20/4/2023"));
-        response.add(new ListItem(2, 1, 4, "item2", "21/5/2023"));
-        response.add(new ListItem(3, 2, 3, "item3", "22/6/2023"));
-        response.add(new ListItem(4, 2, 1, "item4", "23/7/2023"));
+        var itemList = DB.getAll();
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(itemList, HttpStatus.OK);
     }
 
-//    @PostMapping("/{id}")
-//    public ResponseEntity<ItemRecord> store(@PathVariable int id, @RequestBody ItemRecord item) {
-//        if (id != item.id())
-//            return new ResponseEntity<> (item, HttpStatus.BAD_REQUEST);
-//
-//        if (item.status() == null) // Missing parameter status
-//            return new ResponseEntity<> (item, HttpStatus.BAD_REQUEST);
-//
-//        return new ResponseEntity<>(item, HttpStatus.OK);
-//    }
+    @PostMapping("/todo")
+    public ResponseEntity<ListItem> store(@RequestBody ListItem item) {
+        DB.addItem(item);
+
+        return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+
+    @GetMapping("todo/{id}")
+    public ResponseEntity<ListItem> show(@PathVariable int id) {
+
+        ListItem item;
+        try {
+            item = DB.getItem(id);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(item, HttpStatus.OK);
+    }
 }
